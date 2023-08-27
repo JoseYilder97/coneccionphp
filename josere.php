110fe2893690039c1829518086282804ee532829
Jose Gilder <joseyilder1997@gmail.com>
2023-08-27 02:32:57 -0500

    agregas un nuevo titulo de prueba

--- josere.php
+++ josere.php
@@ -8,7 +8,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;500;700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Quicksand:wght@300;500;700&family=Roboto:ital,wght@0,100;0,300;0,500;0,700;1,100;1,400;1,900&display=swap" rel="stylesheet">
-  <title>Este es un  login</title>
+  <title>nuevo  cambio </title>
 </head>
 
 <body>
